ddb9febdc6b7bb965ee2b6b511390b3984ae645a
Grisha Pushkov <grisha@push.org.ru>
2015-04-03 21:18:10 +0300

    dot-dot-dot

---
+++ zsh/custom.zsh
@@ -0,0 +1,20 @@
+# aliases
+alias socks="ssh -Nn -D 1080 vps.push.org.ru"
+alias p="cd ~/Projects"
+alias e="vim"
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias update="sudo brew update; brew upgrade; sudo npm install -g npm; sudo npm update -g"
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
+# locale
+export LANG="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="ru_RU.UTF-8"
+
+# vi mode
+export KEYTIMEOUT=1
