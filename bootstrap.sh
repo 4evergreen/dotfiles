c459fbd64ea9454cece417febacd4409fcd1def3
Grisha Pushkov <grisha@push.org.ru>
2016-11-09 11:56:58 +0300

    (vimperator): remove

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,9 +19,6 @@ ln -s $DOTFILES/vim/snippets ~/.vim/snippets
 mkdir ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# vimperator
-ln -s $DOTFILES/vimperator/vimperatorrc ~/.vimperatorrc
-
 # atom
 mkdir ~/.atom
 ln -s $DOTFILES/atom/config.cson ~/.atom/config.cson
